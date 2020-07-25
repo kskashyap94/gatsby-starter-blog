d02bb084df2b3c9d3cffea275453dedcb6ff0bc5
Kulkarni Shravan kashyap <kskashyap94@gmail.com>
2020-07-25 15:45:10 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Shravan Kashyap's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Shravan Kashyap`,
+      summary: `who lives and works in India building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://kskashyap94.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kskashyap94`,
     },
   },
   plugins: [
